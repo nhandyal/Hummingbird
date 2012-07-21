3de3156aa520d82d6f3039d29491d4852efdc844
Nikhil Handyal <nhandyal@gmail.com>
2012-07-21 01:29:25 -0700

    Added 'add' button and click listener
    Added 'plus-icon' and event listener

--- content_script/script.js
+++ content_script/script.js
@@ -5,5 +5,44 @@ var departmentName = pgURL.substr(6);
 
 
 $.get("http://web-app.usc.edu/ws/soc/api/classes/"+departmentName+"/"+termNumber,{},function(response){
+		var jsonResponse = JSON.parse(response);
+		for (var courseIndex = 0; courseIndex < (jsonResponse.OfferedCourses.course).length; courseIndex++){
+				var sectionDataLength = jsonResponse.OfferedCourses.course[courseIndex].CourseData.SectionData.length;
+				if(sectionDataLength == undefined){
+						// only one offered section
+						var sectionData = jsonResponse.OfferedCourses.course[courseIndex].CourseData.SectionData;
+						var id = sectionData.id;
+						if(sectionData.number_registered >= sectionData.spaces_available){
+								var iconHTML = "<div class='plus-icon-hummingbird'>";
+								iconHTML += "<input type='hidden' class='sectionNumber' value='"+id+"' />";
+								iconHTML += "<input type='hidden' class='courseIndex' value='"+courseIndex+"'/>";
+								iconHTML += "<input type='hidden' class='sectionIndex' value='-1'/></div>";
+								$("#"+id+" > .section").append(iconHTML);
+						}
+				}
+				else{
+						// multiple offered sections
+						for (var sectionIndex = 0; sectionIndex < sectionDataLength; sectionIndex++){
+								var sectionData = jsonResponse.OfferedCourses.course[courseIndex].CourseData.SectionData[sectionIndex];
+								var id = sectionData.id;
+								if(sectionData.number_registered >= sectionData.spaces_available){
+										var iconHTML = "<div class='plus-icon-hummingbird'>";
+										iconHTML += "<input type='hidden' class='sectionNumber' value='"+id+"' />";
+										iconHTML += "<input type='hidden' class='courseIndex' value='"+courseIndex+"'/>";
+										iconHTML += "<input type='hidden' class='sectionIndex' value='"+sectionIndex+"'/></div>";
+										$("#"+id+" > .section").append(iconHTML);
+								}
+						}
+				}
+		}
+		var localPath = chrome.extension.getURL("images/plus-icon.png");
+		localPath = "url('"+localPath+"')";
+		$('.plus-icon-hummingbird').css({"background": localPath});
 		
+		// add click event listener to plus-icon-hummingbird class and add class
+		$('.plus-icon-hummingbird').click(function(){
+				var sectionNumber = $(this).children('.sectionNumber').val();
+				var courseIndex = $(this).children('.courseIndex').val();
+				var sectionIndex = $(this).children('.sectionIndex').val();
+		});
 });
