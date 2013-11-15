78afddea36cb117cc8bfb20c9fce5391ab773479
Nikhil Handyal <nhandyal@gmail.com>
2013-11-14 17:09:39 -0800

    Updated Cron Functions

--- coursespot_transition/website/contact-support.php
+++ coursespot_transition/website/contact-support.php
@@ -6,10 +6,17 @@
 		$message = $_POST['name']." -- ".$_POST['email']." -- ".date("m-d-y -- h:i A" ,time())."\n".$_POST['message'];
 		$headers = "From: ".$_POST['email'] . "\r\n";
 		
+
+		// text notify for support
+		$to_support = "4084276883@txt.att.net";
+		$message_support = "New Support email";
+
 		if(mail($to,$subject,$message,$headers)){
 				$response['status'] = 0;
 				$response['message'] = "<div id='serv-response'>Thank you for your comments. We value your feedback and hope to make Coursespot better with your support.</div>";
 				echo json_encode($response);
+				
+				mail($to_support, $message);
 				exit(0);
 		}
 		else{
